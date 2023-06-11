61b6f3f15eb75f188facedbce508b2dc000209af
itschuk <chuk061123@gmail.com>
2023-06-11 14:14:14 -0700

    update path

--- internal/net_logging.cc
+++ internal/net_logging.cc
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include "tensorflow_serving/util/net_http/internal/net_logging.h"
+#include "net_http/internal/net_logging.h"
 
 #include <stddef.h>
 
