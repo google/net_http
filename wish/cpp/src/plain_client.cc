7c563e9139955a14166c07afce20fe2e3cd4cf97
Takeshi Yoshino <4511440+tyoshino@users.noreply.github.com>
2026-04-07 13:46:15 +0000

    feat(wish/python): add a plain-text mode support

---
+++ wish/cpp/src/plain_client.cc
@@ -0,0 +1,80 @@
+#include "plain_client.h"
+
+#include <iostream>
+
+PlainClient::PlainClient(const std::string& host, int port)
+    : host_(host),
+      port_(port),
+      base_(nullptr),
+      dns_base_(nullptr),
+      handler_(nullptr) {}
+
+PlainClient::~PlainClient() {
+  if (dns_base_) {
+    evdns_base_free(dns_base_, 0);
+  }
+  if (base_) {
+    event_base_free(base_);
+  }
+}
+
+bool PlainClient::Init() {
+  base_ = event_base_new();
+  if (!base_) {
+    std::cerr << "Could not initialize libevent!" << std::endl;
+    return false;
+  }
+
+  dns_base_ = evdns_base_new(base_, 1);
+  if (!dns_base_) {
+    std::cerr << "Could not initialize dns!" << std::endl;
+    return false;
+  }
+
+  struct bufferevent* bev =
+      bufferevent_socket_new(base_, -1, BEV_OPT_CLOSE_ON_FREE);
+  if (!bev) {
+    std::cerr << "Could not create bufferevent!" << std::endl;
+    return false;
+  }
+
+  if (bufferevent_socket_connect_hostname(bev, dns_base_, AF_INET,
+                                          host_.c_str(), port_) < 0) {
+    std::cerr << "Could not connect!" << std::endl;
+    return false;
+  }
+
+  handler_ = new WishHandler(bev, false);
+
+  if (on_open_) {
+    handler_->SetOnOpen([this]() { on_open_(handler_); });
+  }
+
+  if (on_message_) {
+    handler_->SetOnMessage(on_message_);
+  }
+
+  handler_->Start();
+
+  return true;
+}
+
+void PlainClient::SetOnOpen(OpenCallback cb) {
+  on_open_ = cb;
+  if (handler_) {
+    handler_->SetOnOpen([this]() { on_open_(handler_); });
+  }
+}
+
+void PlainClient::SetOnMessage(MessageCallback cb) {
+  on_message_ = cb;
+  if (handler_) {
+    handler_->SetOnMessage(on_message_);
+  }
+}
+
+void PlainClient::Run() {
+  std::cout << "Client running..." << std::endl;
+
+  event_base_dispatch(base_);
+}
---
+++ wish/cpp/src/plain_client.h
@@ -0,0 +1,39 @@
+#ifndef WISH_CPP_SRC_PLAIN_CLIENT_H_
+#define WISH_CPP_SRC_PLAIN_CLIENT_H_
+
+#include <event2/bufferevent.h>
+#include <event2/dns.h>
+#include <event2/event.h>
+
+#include <functional>
+#include <string>
+
+#include "wish_handler.h"
+
+class PlainClient {
+ public:
+  using OpenCallback = std::function<void(WishHandler*)>;
+  using MessageCallback = std::function<void(uint8_t, const std::string&)>;
+
+  PlainClient(const std::string& host, int port);
+  ~PlainClient();
+
+  bool Init();
+  void SetOnOpen(OpenCallback cb);
+  void SetOnMessage(MessageCallback cb);
+  void Run();
+
+ private:
+  std::string host_;
+  int port_;
+
+  struct event_base* base_;
+  struct evdns_base* dns_base_;
+
+  WishHandler* handler_;
+
+  OpenCallback on_open_;
+  MessageCallback on_message_;
+};
+
+#endif  // WISH_CPP_SRC_PLAIN_CLIENT_H_
