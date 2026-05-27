ef0e24597fcb045ea79938451ca1fd1029685da5
nlattice <280584158+nlattice@users.noreply.github.com>
2026-05-27 07:48:43 +0000

    chore(wish/cpp): style fix

--- wish/cpp/src/wish_handler_test.cc
+++ wish/cpp/src/wish_handler_test.cc
@@ -53,12 +53,16 @@ TEST_F(WishHandlerTest, HandshakeAndSimpleExchange) {
   };
 
   server->SetOnMessage([&](uint8_t opcode, const std::string& msg) {
-    if (opcode == WISH_OPCODE_TEXT) received_from_client = msg;
+    if (opcode == WISH_OPCODE_TEXT) {
+      received_from_client = msg;
+    }
     check_done();
   });
 
   client->SetOnMessage([&](uint8_t opcode, const std::string& msg) {
-    if (opcode == WISH_OPCODE_TEXT) received_from_server = msg;
+    if (opcode == WISH_OPCODE_TEXT) {
+      received_from_server = msg;
+    }
     check_done();
   });
 
